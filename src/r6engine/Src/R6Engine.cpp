1e5548d03e5fd8149c9d82d569d9ec317c4b72ad
Daniel Powell <danpowell88@gmail.com>
2026-03-09 17:10:11 +1000

    Batch 23: Implement AR6RagDoll physics methods
    - AddImpulseToBone: remaps bone indices to ragdoll particle indices via switch table, then adds impulse vector to matching particle's position
    - AddSpring: adds a spring constraint between two particles with min/max distance thresholds (squared), -1.0 sentinel means unconstrained
    - ClipParticleToPlane: pushes particle origin back 20% of penetration depth when behind a plane (signed distance < 0)

--- src/r6engine/Src/R6Engine.cpp
+++ src/r6engine/Src/R6Engine.cpp
@@ -2844,16 +2844,69 @@ void AR6PlayerController::execUpdateSpectatorReticule(FFrame& Stack, RESULT_DECL
 
 // --- AR6RagDoll ---
 
-void AR6RagDoll::AddImpulseToBone(INT, FVector)
+void AR6RagDoll::AddImpulseToBone(INT BoneIndex, FVector Impulse)
 {
+	// Remap certain bone indices to their parent ragdoll particle
+	switch (BoneIndex)
+	{
+	case 2: case 4:
+		BoneIndex = 3;
+		break;
+	case 7: case 8: case 9:
+		BoneIndex = 6;
+		break;
+	case 10: case 15:
+		BoneIndex = 5;
+		break;
+	case 14: case 19: case 23: case 27:
+		BoneIndex = BoneIndex - 1;
+		break;
+	}
+
+	for (INT i = 0; i < 16; i++)
+	{
+		if (m_aParticle[i].iRefBone == BoneIndex)
+		{
+			m_aParticle[i].cCurrentPos.Origin.X += Impulse.X;
+			m_aParticle[i].cCurrentPos.Origin.Y += Impulse.Y;
+			m_aParticle[i].cCurrentPos.Origin.Z += Impulse.Z;
+			return;
+		}
+	}
 }
 
-void AR6RagDoll::AddSpring(INT, INT, FLOAT, FLOAT)
+void AR6RagDoll::AddSpring(INT idx1, INT idx2, FLOAT dist, FLOAT maxDist)
 {
+	INT i = m_aSpring.Add(1);
+	FSTSpring& s = m_aSpring(i);
+	s.iFirst = idx1;
+	s.iSecond = idx2;
+
+	if (dist != -1.0f)
+		s.fMinSquared = dist * dist;
+	else
+		s.fMinSquared = -1.0f;
+
+	if (maxDist == 0.0f)
+		s.fMaxSquared = s.fMinSquared;
+	else if (maxDist == -1.0f)
+		s.fMaxSquared = -1.0f;
+	else
+		s.fMaxSquared = maxDist * maxDist;
 }
 
-void AR6RagDoll::ClipParticleToPlane(INT, FVector const &, FVector const &)
+void AR6RagDoll::ClipParticleToPlane(INT particleIdx, FVector const & Normal, FVector const & PlanePoint)
 {
+	FVector& Origin = m_aParticle[particleIdx].cCurrentPos.Origin;
+	FLOAT dist = (Origin.X * Normal.X + Origin.Y * Normal.Y + Origin.Z * Normal.Z)
+	            - (PlanePoint.X * Normal.X + PlanePoint.Y * Normal.Y + PlanePoint.Z * Normal.Z);
+	if (dist < 0.0f)
+	{
+		dist *= 0.2f;
+		Origin.X -= dist * Normal.X;
+		Origin.Y -= dist * Normal.Y;
+		Origin.Z -= dist * Normal.Z;
+	}
 }
 
 void AR6RagDoll::CollisionDetection()
