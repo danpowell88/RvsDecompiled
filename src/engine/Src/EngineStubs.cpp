b779c74bb1dfeb1b9cf956446f0161fb195e9005
Daniel Powell <danpowell88@gmail.com>
2026-03-12 01:15:12 +1000

    Batch 105: Fix copy ctors + default ctors + destructors for UTerrainBrush family
    UTerrainBrush        - FString@+4, FString@+10, 19 DWORDs@+1C  (Ghidra 0x15690)
    UTerrainBrushColor, UTerrainBrushEdgeTurn, UTerrainBrushFlatten,
    UTerrainBrushNoise, UTerrainBrushPaint, UTerrainBrushPlanningPaint,
    UTerrainBrushSelect, UTerrainBrushSmooth, UTerrainBrushTexPan,
    UTerrainBrushTexRotate, UTerrainBrushTexScale, UTerrainBrushVertexEdit,
    UTerrainBrushVisibility  - same layout as UTerrainBrush base class
    
    All 13 subclasses share identical copy ctor / default ctor / dtor bodies;
    linker ICF will merge them (matching retail binary behavior at 0x15b20).

--- src/engine/Src/EngineStubs.cpp
+++ src/engine/Src/EngineStubs.cpp
@@ -6101,16 +6101,26 @@ void UTerrainBrush::MouseMove(float,float)
 {
 }
 
-UTerrainBrush::UTerrainBrush(UTerrainBrush const &)
+UTerrainBrush::UTerrainBrush(UTerrainBrush const &Other)
 {
+	// Ghidra 0x15690: vtable set by compiler; FString placement new at +4 and +10; 19 DWORDs at +1C..+64
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrush::UTerrainBrush()
 {
+	// Initialize 2 FStrings to empty
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrush::~UTerrainBrush()
 {
+	// Ghidra 0x165170: ~FString at +10 then +4 (reverse order)
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrush& UTerrainBrush::operator=(const UTerrainBrush& Other)
@@ -6141,16 +6151,24 @@ FBox UTerrainBrush::GetRect()
 }
 
 // --- UTerrainBrushColor ---
-UTerrainBrushColor::UTerrainBrushColor(UTerrainBrushColor const &)
+UTerrainBrushColor::UTerrainBrushColor(UTerrainBrushColor const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushColor::UTerrainBrushColor()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushColor::~UTerrainBrushColor()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushColor& UTerrainBrushColor::operator=(const UTerrainBrushColor& Other)
@@ -6165,16 +6183,24 @@ void UTerrainBrushColor::Execute(int)
 }
 
 // --- UTerrainBrushEdgeTurn ---
-UTerrainBrushEdgeTurn::UTerrainBrushEdgeTurn(UTerrainBrushEdgeTurn const &)
+UTerrainBrushEdgeTurn::UTerrainBrushEdgeTurn(UTerrainBrushEdgeTurn const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushEdgeTurn::UTerrainBrushEdgeTurn()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushEdgeTurn::~UTerrainBrushEdgeTurn()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushEdgeTurn& UTerrainBrushEdgeTurn::operator=(const UTerrainBrushEdgeTurn& Other)
@@ -6194,16 +6220,24 @@ FBox UTerrainBrushEdgeTurn::GetRect()
 }
 
 // --- UTerrainBrushFlatten ---
-UTerrainBrushFlatten::UTerrainBrushFlatten(UTerrainBrushFlatten const &)
+UTerrainBrushFlatten::UTerrainBrushFlatten(UTerrainBrushFlatten const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushFlatten::UTerrainBrushFlatten()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushFlatten::~UTerrainBrushFlatten()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushFlatten& UTerrainBrushFlatten::operator=(const UTerrainBrushFlatten& Other)
@@ -6218,16 +6252,24 @@ void UTerrainBrushFlatten::Execute(int)
 }
 
 // --- UTerrainBrushNoise ---
-UTerrainBrushNoise::UTerrainBrushNoise(UTerrainBrushNoise const &)
+UTerrainBrushNoise::UTerrainBrushNoise(UTerrainBrushNoise const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushNoise::UTerrainBrushNoise()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushNoise::~UTerrainBrushNoise()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushNoise& UTerrainBrushNoise::operator=(const UTerrainBrushNoise& Other)
@@ -6242,16 +6284,24 @@ void UTerrainBrushNoise::Execute(int)
 }
 
 // --- UTerrainBrushPaint ---
-UTerrainBrushPaint::UTerrainBrushPaint(UTerrainBrushPaint const &)
+UTerrainBrushPaint::UTerrainBrushPaint(UTerrainBrushPaint const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushPaint::UTerrainBrushPaint()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushPaint::~UTerrainBrushPaint()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushPaint& UTerrainBrushPaint::operator=(const UTerrainBrushPaint& Other)
@@ -6270,16 +6320,24 @@ void UTerrainBrushPlanningPaint::MouseButtonDown(UViewport *)
 {
 }
 
-UTerrainBrushPlanningPaint::UTerrainBrushPlanningPaint(UTerrainBrushPlanningPaint const &)
+UTerrainBrushPlanningPaint::UTerrainBrushPlanningPaint(UTerrainBrushPlanningPaint const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushPlanningPaint::UTerrainBrushPlanningPaint()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushPlanningPaint::~UTerrainBrushPlanningPaint()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushPlanningPaint& UTerrainBrushPlanningPaint::operator=(const UTerrainBrushPlanningPaint& Other)
@@ -6302,16 +6360,24 @@ void UTerrainBrushSelect::MouseMove(float,float)
 {
 }
 
-UTerrainBrushSelect::UTerrainBrushSelect(UTerrainBrushSelect const &)
+UTerrainBrushSelect::UTerrainBrushSelect(UTerrainBrushSelect const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushSelect::UTerrainBrushSelect()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushSelect::~UTerrainBrushSelect()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushSelect& UTerrainBrushSelect::operator=(const UTerrainBrushSelect& Other)
@@ -6331,16 +6397,24 @@ FBox UTerrainBrushSelect::GetRect()
 }
 
 // --- UTerrainBrushSmooth ---
-UTerrainBrushSmooth::UTerrainBrushSmooth(UTerrainBrushSmooth const &)
+UTerrainBrushSmooth::UTerrainBrushSmooth(UTerrainBrushSmooth const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushSmooth::UTerrainBrushSmooth()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushSmooth::~UTerrainBrushSmooth()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushSmooth& UTerrainBrushSmooth::operator=(const UTerrainBrushSmooth& Other)
@@ -6359,16 +6433,24 @@ void UTerrainBrushTexPan::MouseMove(float,float)
 {
 }
 
-UTerrainBrushTexPan::UTerrainBrushTexPan(UTerrainBrushTexPan const &)
+UTerrainBrushTexPan::UTerrainBrushTexPan(UTerrainBrushTexPan const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushTexPan::UTerrainBrushTexPan()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushTexPan::~UTerrainBrushTexPan()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushTexPan& UTerrainBrushTexPan::operator=(const UTerrainBrushTexPan& Other)
@@ -6383,16 +6465,24 @@ void UTerrainBrushTexRotate::MouseMove(float,float)
 {
 }
 
-UTerrainBrushTexRotate::UTerrainBrushTexRotate(UTerrainBrushTexRotate const &)
+UTerrainBrushTexRotate::UTerrainBrushTexRotate(UTerrainBrushTexRotate const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushTexRotate::UTerrainBrushTexRotate()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushTexRotate::~UTerrainBrushTexRotate()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushTexRotate& UTerrainBrushTexRotate::operator=(const UTerrainBrushTexRotate& Other)
@@ -6407,16 +6497,24 @@ void UTerrainBrushTexScale::MouseMove(float,float)
 {
 }
 
-UTerrainBrushTexScale::UTerrainBrushTexScale(UTerrainBrushTexScale const &)
+UTerrainBrushTexScale::UTerrainBrushTexScale(UTerrainBrushTexScale const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushTexScale::UTerrainBrushTexScale()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushTexScale::~UTerrainBrushTexScale()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushTexScale& UTerrainBrushTexScale::operator=(const UTerrainBrushTexScale& Other)
@@ -6427,16 +6525,24 @@ UTerrainBrushTexScale& UTerrainBrushTexScale::operator=(const UTerrainBrushTexSc
 }
 
 // --- UTerrainBrushVertexEdit ---
-UTerrainBrushVertexEdit::UTerrainBrushVertexEdit(UTerrainBrushVertexEdit const &)
+UTerrainBrushVertexEdit::UTerrainBrushVertexEdit(UTerrainBrushVertexEdit const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushVertexEdit::UTerrainBrushVertexEdit()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushVertexEdit::~UTerrainBrushVertexEdit()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushVertexEdit& UTerrainBrushVertexEdit::operator=(const UTerrainBrushVertexEdit& Other)
@@ -6447,16 +6553,24 @@ UTerrainBrushVertexEdit& UTerrainBrushVertexEdit::operator=(const UTerrainBrushV
 }
 
 // --- UTerrainBrushVisibility ---
-UTerrainBrushVisibility::UTerrainBrushVisibility(UTerrainBrushVisibility const &)
+UTerrainBrushVisibility::UTerrainBrushVisibility(UTerrainBrushVisibility const &Other)
 {
+	// Same layout as UTerrainBrush; vtable set by compiler
+	new ((BYTE*)this + 0x04) FString(*(const FString*)((const BYTE*)&Other + 0x04));
+	new ((BYTE*)this + 0x10) FString(*(const FString*)((const BYTE*)&Other + 0x10));
+	appMemcpy((BYTE*)this + 0x1C, (const BYTE*)&Other + 0x1C, 0x4C); // 19 DWORDs
 }
 
 UTerrainBrushVisibility::UTerrainBrushVisibility()
 {
+	new ((BYTE*)this + 0x04) FString();
+	new ((BYTE*)this + 0x10) FString();
 }
 
 UTerrainBrushVisibility::~UTerrainBrushVisibility()
 {
+	((FString*)((BYTE*)this + 0x10))->~FString();
+	((FString*)((BYTE*)this + 0x04))->~FString();
 }
 
 UTerrainBrushVisibility& UTerrainBrushVisibility::operator=(const UTerrainBrushVisibility& Other)
